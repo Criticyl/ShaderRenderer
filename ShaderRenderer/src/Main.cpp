6888f34492249fdfe083cd496cde5817e890d995
Criticyl <criticyl@gmail.com>
2025-09-08 19:15:32 +0100

    Updated CMake file, also added glad as a vendor instead.

--- ShaderRenderer/src/Main.cpp
+++ ShaderRenderer/src/Main.cpp
@@ -1,2 +1,6 @@
 #include <iostream>
 
+#include <glm/glm.hpp>
+#include <glad/glad.h>
+
+int main()
---
+++ ShaderRenderer/src/Renderer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+class Renderer
+{
+private:
+    std::uint_32t m_Window;
+};
